2ee425902a6573d1e2c428c26ab31c450ff1d452
codingluke <lukashodel@gmail.com>
2014-05-05 17:26:03 +0200

    Featurecomplete!

--- aufgabe_3/hello_world.cpp
+++ aufgabe_3/hello_world.cpp
@@ -14,6 +14,33 @@ void print_actions()
   cout << "3) Programm beenden\n";
 }
 
+void generate_and_print_calendar()
+{
+  int month = read_int("Monat angeben:\t", 1, 12);
+  int year = read_int("Jahr angeben:\t", 1583, 1000000);
+  cout << "\n";
+  print_calender(month, year);
+  cout << "\n\n";
+}
+
+void calc_and_print_days_between_dates()
+{
+  cout << "\nErstes Datum angeben:\n";
+  Date date1;
+  date1.day = read_int("Tag:\t", 1, 12);
+  date1.month = read_int("Monat:\t", 1, 12);
+  date1.year = read_int("Jahr:\t", 1583, 1000000);
+  cout << "Zweites Datum angeben:\n";
+  Date date2;
+  date2.day = read_int("Tag:\t", 1, 12);
+  date2.month = read_int("Monat:\t", 1, 12);
+  date2.year = read_int("Jahr:\t", 1583, 1000000);
+  int days_bewteen = calc_days_between_dates(date1, date2);
+  cout << "Tage zwischen den daten: ";
+  write_number((long)days_bewteen, cout.left);
+  cout << "\n\n";
+}
+
 int main()
 {
   int action = 0;
@@ -22,30 +49,9 @@ int main()
     print_actions();
     action = read_int("Bitte waehlen:\t", 1, 3);
     if (action == 1)
-    {
-      int month = read_int("Monat angeben:\t", 1, 12);
-      int year = read_int("Jahr angeben:\t", 1583, 1000000);
-      cout << "\n";
-      print_calender(month, year);
-      cout << "\n\n";
-    }
+      generate_and_print_calendar();
     else if (action == 2)
-    {
-      cout << "\nErstes Datum angeben:\n";
-      Date date1;
-      date1.day = read_int("Tag:\t", 1, 12);
-      date1.month = read_int("Monat:\t", 1, 12);
-      date1.year = read_int("Jahr:\t", 1583, 1000000);
-      cout << "Zweites Datum angeben:\n";
-      Date date2;
-      date2.day = read_int("Tag:\t", 1, 12);
-      date2.month = read_int("Monat:\t", 1, 12);
-      date2.year = read_int("Jahr:\t", 1583, 1000000);
-      int days_bewteen = calc_days_between_dates(date1, date2);
-      cout << "Tage zwischen den daten: ";
-      write_number((long)days_bewteen, cout.left);
-      cout << "\n\n";
-    }
+      calc_and_print_days_between_dates();
   } while (action != 3);
   cout << "Byebye";
 }
