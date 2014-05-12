bf1cd471751c5e262ecac5c3c2510acb9ccd4658
codingluke <lukashodel@gmail.com>
2014-05-12 20:41:04 +0200

    Commenting

--- aufgabe_4/table_main.cpp
+++ aufgabe_4/table_main.cpp
@@ -10,15 +10,17 @@
 
 using namespace std;
 
+const int BUFFSIZE = 10;
 const ptMathFunction functions[10] = {
   &cos, &sin, &tan, &acos, &asin, &atan,
   &exp, &log, &log10, &sqrt
 };
-
-const string function_names[11] = {
+const string function_names[10] = {
   "cos", "sin", "tan", "acos", "asin", "atan",
-  "exp", "log", "log10", "sqrt", "pow"
+  "exp", "log", "log10", "sqrt",
 };
+const ptMathFunction2 functions2[1] = { &pow };
+const string function2_names[1] = { "pow" };
 
 int main(int argc, char *argv[])
 {
@@ -35,29 +37,32 @@ int main(int argc, char *argv[])
   }
   else
     if (validate_input(argc, argv))
-      handle_action(argc, argv);
+      handle_action(argv);
     else
       cout << "failure!";
 }
 
-void handle_action(int argc, char *argv[])
+void handle_action(char *argv[])
 {
-  int function_index = get_function_index(argv[1]);
+  string type = get_function_type(argv[1]);
   string table;
   string filename;
-  if (argc == 9)
+  int function_index;
+  if (type == "one")
   {
-    table = generate_table(&pow, argv[1], atof(argv[2]), atof(argv[3]),
-                           atof(argv[4]), atof(argv[5]), atof(argv[6]),
-                           atof(argv[7]));
-    filename = argv[8];
-  }
-  else if (argc == 8)
-  {
+    function_index = get_array_index(argv[1], function_names, 11);
     table = generate_table(functions[function_index],
                            argv[1], atof(argv[2]), atof(argv[3]),
                            atof(argv[4]), atof(argv[5]), atof(argv[6]));
     filename = argv[7];
+  }
+  else if (type == "two")
+  {
+    function_index = get_array_index(argv[1], function2_names, 1);
+    table = generate_table(functions2[function_index], argv[1], atof(argv[2]), atof(argv[3]),
+                           atof(argv[4]), atof(argv[5]), atof(argv[6]),
+                           atof(argv[7]));
+    filename = argv[8];
   }
   cout << table;
   if (file_exists(filename))
@@ -72,78 +77,146 @@ void handle_action(int argc, char *argv[])
     write_to_file(filename, table);
 }
 
-/**
- * Prints a description of the available actions to the console.
- */
-void print_actions()
-{
-  cout << "Waehle die Funktion zur Tabellengenerierung:" << endl;
-  for (int i = 0; i < 11 ; i++)
-    cout << setw(2) << left << i + 1 << ") " << function_names[i] << endl;
-}
-
-int get_function_index(string function_name)
-{
-  int index;
-  for (int i = 0; i < 11 ; i++)
-    if (function_names[i] == function_name)
-      index = i;
-  return index;
-}
-
 void handle_action(int action)
 {
   action--; // because an array starts with 0
   string table;
   if (action < 10)
     table = generate_table(functions[action], function_names[action]);
-  else if (action == 10)
-    table = generate_table(&pow, function_names[action]);
+  else if (action > 9)
+    table = generate_table(functions2[action - 10], function2_names[action - 10]);
   cout << table;
   write_to_file(table);
 }
 
+/**
+ * Prints a description of the available actions to the console.
+ */
+void print_actions()
+{
+  cout << "Waehle die Funktion zur Tabellengenerierung:" << endl;
+  for (int i = 0; i < 10 ; i++)
+    cout << setw(2) << left << i + 1 << ") " << function_names[i] << endl;
+  for (int i = 0; i < 1 ; i++)
+    cout << setw(2) << left << i + 11 << ") " << function2_names[i] << endl;
+}
+
+/**
+ * Returns the index of a string inside an array of strings.
+ *
+ * @param item  String to be searched in the array.
+ * @param items Array which contains the item.
+ *
+ * @return int index of the item in the array.
+ *             -1 if not in the array.
+ */
+int get_array_index(string item, const string items[], int length)
+{
+  int index = -1;
+  for (int i = 0; i < length ; i++)
+    if (items[i] == item)
+      index = i;
+  return index;
+}
+
+/**
+ * Returns a string representing the type of a function by its name.
+ * There are two defined types ("one" and "two").
+ *
+ * @param name String Name of the function to be checked.
+ *
+ * @return String one ptMathFunction:  double (*)(double)
+ *                two ptMathFunction2: double (*)(double, double)
+ */
+string get_function_type(string name)
+{
+  string type = "one";
+  for (int i = 0; i < 1 ; i++)
+    if (function2_names[i] == name)
+      type = "two";
+  return type;
+}
+
+/**
+ * Validates the input arguments.
+ * Checks if the function is available.
+ * Checks if there are enough argumends for the
+ * given function type.
+ * Checks if the arguments are in the right types.
+ *
+ * @param argc    int lenght of the argument array.
+ * @param argv[]  *char argument array.
+ *
+ * @return true   when the arguments are valid.
+ *         false  when the arguments are invalid.
+ */
 bool validate_input(int argc, char *argv[])
 {
-  cout << argc;
   bool valid = false;
   valid = validate_function_name(argv[1]);
-  valid = valid && validate_params_length(argv[1], argc);
-  valid = valid && validate_param_types(argv[1], argv);
+  string function_type = get_function_type(argv[1]);
+  valid = valid && validate_params_length(function_type, argc);
+  valid = valid && validate_param_types(function_type, argv);
   return valid;
 }
 
-bool validate_param_types(string function_name, char *argv[])
+/**
+ * Validates the type of the arguments according the type of the given
+ * function to be called.
+ *
+ * @param function_type String Type of the function ("one" ore "two")
+ * @param argv[]        *char  input arguments.
+ *
+ * @return true  when the argument types fit the functions type.
+ *         false when the argument types don't fit the functions type.
+ */
+bool validate_param_types(string function_type, char *argv[])
 {
   bool valid = false;
-  if (function_name == "pow" && atof(argv[2]) && atof(argv[3])
-      && atof(argv[4]) && atof(argv[4]) && atof(argv[5])
-      && atof(argv[6]) && atof(argv[7]) && !atof(argv[8]))
-    valid = true;
-  else if (function_name != "pow" && atof(argv[2]) && atof(argv[3])
+  if (function_type == "one" && atof(argv[2]) && atof(argv[3])
            && atof(argv[4]) && atof(argv[4]) && atof(argv[5])
            && atof(argv[6]) && !atof(argv[7]))
     valid = true;
+  else if (function_type == "two" && atof(argv[2]) && atof(argv[3])
+      && atof(argv[4]) && atof(argv[4]) && atof(argv[5])
+      && atof(argv[6]) && atof(argv[7]) && !atof(argv[8]))
+    valid = true;
   return valid;
 }
 
-bool validate_params_length(string function_name, int argc)
+/**
+ * Validates the argument length according a given function type.
+ *
+ * @param function_type String Type of the function ("one" ore "two")
+ * @param argc          int    Length of the argument array.
+ *
+ * @return true   if the argument length fit the function type
+ *         false  if the argument length don fit the function type.
+ */
+bool validate_params_length(string function_type, int argc)
 {
   bool valid = false;
-  if (function_name == "pow" && argc == 9)
-    valid = true;
-  else if (function_name != "pow" && argc == 8)
+  if ((function_type == "one" && argc == 8) ||
+      (function_type == "two" && argc == 9))
     valid = true;
   return valid;
 }
 
-bool validate_function_name(string function_name)
+/**
+ * Checks if a function (by its name) is defined.
+ *
+ * @param name String function by name, to be checked.
+ *
+ * @return true   if function exists.
+ *         fales  if function don't exists.
+ */
+bool validate_function_name(string name)
 {
   bool valid = false;
   for (int i = 0; i < 11 ; i++)
-    if (function_names[i] == function_name)
+    if (function_names[i] == name)
       valid = true;
   if (!valid)
-    cout << "Nicht gueltige Funktion " << function_name << endl;
+    cout << "Nicht gueltige Funktion '" << name << "'" << endl;
   return valid;
 }
--- aufgabe_4/table_main.h
+++ aufgabe_4/table_main.h
@@ -6,14 +6,16 @@
 using namespace std;
 
 typedef double (*ptMathFunction)(double);
+typedef double (*ptMathFunction2)(double, double);
 int main(int argc, char *argv[]);
 void print_actions();
 void handle_action(int action);
-void handle_action(int argc, char *argv[]);
+void handle_action(char *argv[]);
 bool validate_input(int argc, char *argv[]);
 bool validate_function_name(string function_name);
-bool validate_params_length(string function_name, int argc);
-bool validate_param_types(string function_name, char *argv[]);
-int get_function_index(string function_name);
+bool validate_params_length(string function_type, int argc);
+bool validate_param_types(string function_type, char *argv[]);
+int get_array_index(string name, const string names[], int length);
+string get_function_type(string name);
 
 #endif
