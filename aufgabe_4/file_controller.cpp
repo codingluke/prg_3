9ddb1a60dc28f615fcd02bd7bfff53ca0f2e5a3c
codingluke <lukashodel@gmail.com>
2014-05-14 20:08:59 +0200

    More commenting

--- aufgabe_4/file_controller.cpp
+++ aufgabe_4/file_controller.cpp
@@ -4,23 +4,54 @@
 #include "console_input.h"
 #include "file_controller.h"
 
+/**
+ * Writes given text into a file with the given filename.
+ *
+ * @param filename Name of the file to wirte.
+ * @param text     Text to write into the file.
+ */
 void write_to_file(string filename, string text)
 {
   ofstream outfile(filename.c_str());
   outfile << text;
 }
 
+/**
+ * Writes a given text into a file. The name of the
+ * file has to be entered by the user. When the file
+ * already exists. The user gets prompted to overwrite
+ * or entering a new name.
+ *
+ * @param text     Text to write into the file.
+ */
 void write_to_file(string text)
 {
   string filename = read_secure_filename();
   write_to_file(filename, text);
 }
 
+/**
+ * Checks if a file already exists by its name.
+ *
+ * @param filename Filename to check for existence.
+ *
+ * @return true   when the file exists.
+ *         false  when the file don't exists.
+ */
 bool file_exists(const std::string& filename) {
   ifstream ifile(filename.c_str());
   return ifile;
 }
 
+/**
+ * Promts the user to enter a filename. The filename gets checked
+ * for existance. When there is already an existing file for
+ * the enterd filename, the user gets promted to override
+ * or reenter e new name.
+ *
+ * @return a filename of a new file or of an existing file which
+ *         can be overwritten.
+ */
 string read_secure_filename()
 {
   string filename;
